b0db3d814a8f7bf518fbb73ea8043788cf101edf
Damien Pichard <damienpichard@tuta.com>
2024-12-16 23:50:15 +0100

    Add support for MacPorts

--- shell/bash/plugins/mac/mac.plugin.bash
+++ shell/bash/plugins/mac/mac.plugin.bash
@@ -1,8 +1,8 @@
 ### mac.plugin.bash ---                              -*- mode: shell-script; -*-
 
-## Copyright (C) 2021-2023  damienpichard
+## Copyright (C) 2021-2025  Damien Pichard
 
-## Author: damienpichard <damienpichard@tutanota.de>
+## Author: damienpichard <damienpichard@tuta.com>
 ## Keywords:
 
 ## This program is free software; you can redistribute it and/or modify
@@ -26,34 +26,73 @@
 
 
 
-if assert_eq $SYSTEM macos
-then
-    # Add Homebrew paths to $PATH and $MANPATH
-    export PATH="/usr/local/bin:/usr/local/sbin:$PATH"
-    export MANPATH="$MANPATH:/usr/local/share/man"
-
-    ICLOUD_PATH="${HOME}/Library/Library/Mobile Documents/com~apple~CloudDocs/"
-
-    if assert_command_exists brew
-    then
-        # Add some useful shortcuts as aliases
-        alias buuc="brew update && brew upgrade && brew cleanup"
-        alias brrc="rm -rf ~/Library/Caches/Homebrew/"
-    fi
+if assert_eq $SYSTEM macos; then
+  XCODE_PATH="/Applications/Xcode.app"
+  if assert_directory_exists ${XCODE_PATH}; then
+     # Add XCode toolchain manuals path to $MANPATH
+     export MANPATH="${XCODE_PATH}/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/share/man:${MANPATH}"
+  fi
 
 
-    if assert_directory_exists "/Applications/Xcode.app/"
-    then
-       # Add XCode toolchain manuals path to $MANPATH
-       export MANPATH="$MANPATH:/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/share/man"
-    fi
-
-
+  TEX_PATH="Library/TeX/texbin"
+  if assert_directory_exists ${TEX_PATH}; then
     # Add MacTeX utilities to $PATH when installed
-    if assert_directory_exists "/Library/TeX/"
-    then
-        export PATH="$PATH:/Library/TeX/texbin"
-    fi
+    export PATH="${TEX_PATH}:${PATH}"
+  fi
 
-    alias icloudclear="find ${ICLOUD_PATH} -type f -exec brctl evict {} \;"
+  
+  MACPORTS_BIN_PATH="/opt/local/bin"
+  MACPORTS_SBIN_PATH="/opt/local/sbin"
+  MACPORTS_MAN_PATH="/opt/local/share/man"
+  if assert_directory_exists "${MACPORTS_BIN_PATH}"; then
+    export PATH="${MACPORTS_BIN_PATH}:${MACPORTS_SBIN_PATH}:${PATH}"
+    export MANPATH="${MACPORTS_MAN_PATH}:${PATH}"
+
+    function macports {
+      case "${1}" in
+        info)       port info            ${2} ;;
+        install|i)  sudo port install    ${2} ;;
+        remove|r)   sudo port uninstall  ${2} ;;
+        update|u)   sudo port upgrade    ${2} ;;
+        upgrade|U)  sudo port -u upgrade ${2} ;;
+        search|s)   port search --name --line --regex "${2}" ;;
+        selfupdate) sudo port selfupdate ;;
+      esac
+    }
+  fi
+  
+
+  HOMEBREW_BIN_PATH="/usr/local/bin"
+  HOMEBREW_SBIN_PATH="/usr/local/sbin"
+  HOMEBREW_MAN_PATH="/usr/local/share/man"
+  HOMEBREW_CACHE_PATH="${HOME}/Library/Caches/Homebrew"
+  HOMEBREW_BIN="${HOMEBREW_BIN_PATH}/brew"
+  if assert_file_exists ${HOMEBREW_BIN}; then
+    # Add Homebrew paths to $PATH and $MANPATH
+    export PATH="${HOMEBREW_BIN_PATH}:${HOMEBREW_SBIN_PATH}:${PATH}"
+    export MANPATH="${HOMEBREW_MAN_PATH}:${MANPATH}"
+ 
+    # Add some useful shortcuts as aliases
+    alias buuc="brew update && brew upgrade && brew cleanup"
+    alias brrc="rm -rf ${HOMEBREW_CACHE}"
+  fi
+ 
+
+  ICLOUD_PATH="${HOME}/Library/Mobile Documents/com~apple~CloudDocs/"
+  if assert_directory_exists ${ICLOUD_PATH}; then
+    # Remove / clear iCloud downloads to get more local space without deleting files.
+    alias icloudclear="find '${ICLOUD_PATH}' -type f -exec brctl evict {} \;"
+  fi
+
+
+  function copyfonts {
+    cp -rf "${HOME}/.config/pkgsrc/share/fonts/X11/TTF/" "${HOME}/Library/Fonts/"
+  }
+
+  function set_hostname {
+      sudo scutil --set HostName "${1}"
+      sudo scutil --set LocalHostName "${1}"
+      sudo scutil --set ComputerName "${1}"
+      dscacheutil -flushcache
+  }
 fi
