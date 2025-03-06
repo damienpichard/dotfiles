60964ea883d622c9e53a7bb4d55f4fa152246894
Damien Pichard <damienpichard@tuta.com>
2025-03-06 20:46:32 +0100

    Remove package management from mac plugins

--- shell/bash/plugins/mac/mac.plugin.bash
+++ shell/bash/plugins/mac/mac.plugin.bash
@@ -24,75 +24,38 @@
 
 ### Code:
 
-
-
 if assert_eq $SYSTEM macos; then
-  XCODE_PATH="/Applications/Xcode.app"
-  if assert_directory_exists ${XCODE_PATH}; then
-     # Add XCode toolchain manuals path to $MANPATH
-     export MANPATH="${XCODE_PATH}/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/share/man:${MANPATH}"
-  fi
+    XCODE_PATH="/Applications/Xcode.app"
+    if assert_directory_exists ${XCODE_PATH}; then
+        # Add XCode toolchain manuals path to $MANPATH
+        export MANPATH="${XCODE_PATH}/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/share/man:${MANPATH}"
+    fi
 
+    COMMANDLINE_TOOLS_PATH="/Library/Developer/CommandLineTools/usr/bin"
+    if assert_directory_exists ${COMMANDLINE_TOOLS_PATH}; then
+        export PATH="${COMMANDLINE_TOOLS_PATH}:${PATH}"
+    fi
 
-  TEX_PATH="Library/TeX/texbin"
-  if assert_directory_exists ${TEX_PATH}; then
-    # Add MacTeX utilities to $PATH when installed
-    export PATH="${TEX_PATH}:${PATH}"
-  fi
+    TEX_PATH="Library/TeX/texbin"
+    if assert_directory_exists ${TEX_PATH}; then
+        # Add MacTeX utilities to $PATH when installed
+        export PATH="${TEX_PATH}:${PATH}"
+    fi
 
-  
-  MACPORTS_BIN_PATH="/opt/local/bin"
-  MACPORTS_SBIN_PATH="/opt/local/sbin"
-  MACPORTS_MAN_PATH="/opt/local/share/man"
-  if assert_directory_exists "${MACPORTS_BIN_PATH}"; then
-    export PATH="${MACPORTS_BIN_PATH}:${MACPORTS_SBIN_PATH}:${PATH}"
-    export MANPATH="${MACPORTS_MAN_PATH}:${PATH}"
+    ICLOUD_PATH="${HOME}/Library/Mobile Documents/com~apple~CloudDocs/"
+    if assert_directory_exists ${ICLOUD_PATH}; then
+        # Remove / clear iCloud downloads to get more local space without deleting files.
+        alias icloudclear="find '${ICLOUD_PATH}' -type f -exec brctl evict {} \;"
+    fi
 
-    function macports {
-      case "${1}" in
-        info)       port info            ${2} ;;
-        install|i)  sudo port install    ${2} ;;
-        remove|r)   sudo port uninstall  ${2} ;;
-        update|u)   sudo port upgrade    ${2} ;;
-        upgrade|U)  sudo port -u upgrade ${2} ;;
-        search|s)   port search --name --line --regex "${2}" ;;
-        selfupdate) sudo port selfupdate ;;
-      esac
+    function copyfonts {
+        cp -rf "${HOME}/.config/pkgsrc/share/fonts/X11/TTF/" "${HOME}/Library/Fonts/"
     }
-  fi
-  
 
-  HOMEBREW_BIN_PATH="/opt/homebrew/bin"
-  HOMEBREW_SBIN_PATH="/opt/homebrew/sbin"
-  HOMEBREW_MAN_PATH="/opt/homebrew/share/man"
-  HOMEBREW_CACHE_PATH="${HOME}/Library/Caches/Homebrew"
-  HOMEBREW_BIN="${HOMEBREW_BIN_PATH}/brew"
-  if assert_file_exists ${HOMEBREW_BIN}; then
-    # Add Homebrew paths to $PATH and $MANPATH
-    export PATH="${HOMEBREW_BIN_PATH}:${HOMEBREW_SBIN_PATH}:${PATH}"
-    export MANPATH="${HOMEBREW_MAN_PATH}:${MANPATH}"
- 
-    # Add some useful shortcuts as aliases
-    alias buuc="brew update && brew upgrade && brew cleanup"
-    alias brrc="rm -rf ${HOMEBREW_CACHE}"
-  fi
- 
-
-  ICLOUD_PATH="${HOME}/Library/Mobile Documents/com~apple~CloudDocs/"
-  if assert_directory_exists ${ICLOUD_PATH}; then
-    # Remove / clear iCloud downloads to get more local space without deleting files.
-    alias icloudclear="find '${ICLOUD_PATH}' -type f -exec brctl evict {} \;"
-  fi
-
-
-  function copyfonts {
-    cp -rf "${HOME}/.config/pkgsrc/share/fonts/X11/TTF/" "${HOME}/Library/Fonts/"
-  }
-
-  function set_hostname {
-      sudo scutil --set HostName "${1}"
-      sudo scutil --set LocalHostName "${1}"
-      sudo scutil --set ComputerName "${1}"
-      dscacheutil -flushcache
-  }
+    function set_hostname {
+        sudo scutil --set HostName "${1}"
+        sudo scutil --set LocalHostName "${1}"
+        sudo scutil --set ComputerName "${1}"
+        dscacheutil -flushcache
+    }
 fi
